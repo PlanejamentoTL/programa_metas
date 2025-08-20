e6398f73c1df81b8d1a11fcf3960f1e4c473f716
PlanejamentoTL <planejamento.estatistica@treslagoas.ms.gov.br>
2025-08-20 10:28:57 -0400

    v1.0 - Producao

--- src/components/GrapicsBento/GrapicsBento.jsx
+++ src/components/GrapicsBento/GrapicsBento.jsx
@@ -1,13 +1,51 @@
 import React, { useEffect, useState } from "react";
 import Papa from "papaparse";
-import { Doughnut } from "react-chartjs-2";
+import { Doughnut, Line, Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   ArcElement,
   Tooltip,
   Legend,
 } from "chart.js";
-import { IoDocumentSharp } from "react-icons/io5";
+
+import {
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement,
+} from "chart.js";
+
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement,
+  ChartDataLabels // ← ESSENCIAL para que o plugin funcione!
+);
+
+
+
+import { IoDocumentSharp, IoTrendingUpOutline, IoSchoolOutline, IoBusinessOutline } from "react-icons/io5";
+import { LiaHatCowboySideSolid } from "react-icons/lia";
+import { FaHandsHelping, FaTheaterMasks, FaLeaf } from "react-icons/fa";
+import { RiMoneyDollarCircleLine, RiGovernmentLine } from "react-icons/ri";
+import { BsHouses } from "react-icons/bs";
+import { CiDeliveryTruck } from "react-icons/ci";
+import { MdHealthAndSafety, MdOutlineSportsVolleyball } from "react-icons/md";
+import { PiTrafficSign } from "react-icons/pi";
+
+
+
+
+
+
 
 
 import "./GrapicsBento.css";
@@ -18,12 +56,19 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 const url =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQdKKNMJxtxa0eo3er_xR6MEuoNtEj7m37GWatu6zyzgNhbQpb3E4eY6mmdmqORBVQHbmOhjYfyl7ZB/pub?gid=1952842445&single=true&output=csv";
 
+const url_financeiro = 
+   "https://docs.google.com/spreadsheets/d/e/2PACX-1vRqIsZ9Y0T__qRR4h6k3q6m1B_D6By0MO49t3pBs7vwaUs2wbNljsxMmxQ_TwWGJQB0g8L81ZhTRljD/pub?gid=440817858&single=true&output=csv"
+
 function TabelaEGrafico() {
   const [dados, setDados] = useState([]);
+  const [dados_financeiros, setDados_financeiros] = useState([]);
   const [chartData, setChartData] = useState(null);
   const [filtroSecretaria, setFiltroSecretaria] = useState("Todos");
-  const [filtroPlano, setFiltroPlano] = useState("Todos");
+  
+ 
+  const [filtroPlano, setFiltroPlano] = useState("Plano de Governo");
   const [linhaAberta, setLinhaAberta] = useState(null);
+  const [filtroTipo, setFiltroTipo] = useState("Geral");
 
   useEffect(() => {
     fetch(url)
@@ -33,6 +78,21 @@ function TabelaEGrafico() {
         setDados(parse_data);
       });
   }, []);
+
+
+  useEffect(() => {
+    fetch(url_financeiro)
+      .then((response) => response.text())
+      .then((data) => {
+        const parse_data2 = Papa.parse(data, { header: true }).data;
+        console.log("Dados financeiros brutos:", parse_data2);
+        setDados_financeiros(parse_data2);
+      });
+  }, []);
+
+
+  const anosFixos = ["2021", "2022", "2023", "2024", "2025"];
+
 
   const dadosFiltrados = dados.filter(item => {
     const bySecretaria = filtroSecretaria === "Todos" || item.Secretaria === filtroSecretaria;
@@ -90,6 +150,97 @@ const chartDataObj = {
   ],
 };
 
+const options_porcentagem = {
+  plugins: {
+    legend: {
+      position: 'bottom'
+    },
+    datalabels: {
+      color: '#fff',
+      font: {
+        weight: 'bold'
+      },
+      formatter: (value, context) => {
+        const data = context.chart.data.datasets[0].data;
+        const total = data.reduce((acc, val) => acc + val, 0);
+        const percentage = ((value / total) * 100).toFixed(1);
+        return `${percentage}%`;
+      }
+    }
+  }
+};
+
+
+const agrupadosDotacaoInicial = {};
+const agrupadosDotacaoFinal = {};
+anosFixos.forEach(ano => {
+  agrupadosDotacaoInicial[ano] = 0;
+  agrupadosDotacaoFinal[ano] = 0;
+});
+
+const dadosFinanceirosFiltrados = dados_financeiros.filter((d) => {
+  const byTipo = filtroTipo === "Todos" || d.Tipo === filtroTipo;
+  const bySecretaria = filtroSecretaria === "Todos" || d.Secretaria === filtroSecretaria;
+  return byTipo && bySecretaria;
+});
+
+
+dadosFinanceirosFiltrados.forEach((d) => {
+  const ano = d.Ano?.trim();
+  const valorInicial = parseFloat(d.Dotacao_Inicial?.replace(/\./g, "").replace(",", ".")) || 0;
+  const valorFinal = parseFloat(d.Dotacao_Final?.replace(/\./g, "").replace(",", ".")) || 0;
+
+  if (anosFixos.includes(ano)) {
+    agrupadosDotacaoInicial[ano] += valorInicial;
+    agrupadosDotacaoFinal[ano] += valorFinal;
+  }
+});
+
+
+const dadosLinha = {
+  labels: anosFixos,
+  datasets: [
+    {
+      label: `Dotação Inicial (${filtroTipo})`,
+      data: anosFixos.map(ano => agrupadosDotacaoInicial[ano]),
+      borderColor: '#0087F8',
+      backgroundColor: 'rgba(0, 96, 163)',
+      tension: 0.3,
+      fill: true,
+    },
+    {
+      label: `Dotação Final (${filtroTipo})`,
+      data: anosFixos.map(ano => agrupadosDotacaoFinal[ano]),
+      borderColor: '#F89C00',
+      backgroundColor: 'rgba(248, 156, 0)',
+      tension: 0.3,
+      fill: true,
+    },
+  ],
+};
+
+
+
+
+const opcoesLinha = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'bottom',
+    },
+    title: {
+      display: true,
+      text: 'Investimentos por Ano',
+    },
+
+   datalabels: {
+      display: false, 
+    },
+    
+  },
+};
+
+
 
   const secretarias = Array.from(new Set(dados.map(d => d.Secretaria).filter(Boolean)));
   const planos = Array.from(new Set(dados.map(d => d.Plano).filter(Boolean)));
@@ -106,7 +257,7 @@ const chartDataObj = {
           <div className="bar-flex" > <h3>Painel de Monitoramento - Programa de Metas</h3> </div>
        
 
-
+        <div className="conteudo">
 
       <div className="filtros_div">
         <label  >
@@ -121,8 +272,14 @@ const chartDataObj = {
           Filtrar por Plano:
            <br/>
           <select className = "filtros" value={filtroPlano} onChange={e => setFiltroPlano(e.target.value)}>
-            <option value="Todos">Todos</option>
-            {planos.map((p, i) => <option key={i} value={p}>{p}</option>)}
+            <option value="Plano de Governo">Plano de Governo</option>
+            <option value="Plano Plurianual">Plano Plurianual</option>
+            <option value="Lei de Diretrizes Orçamentárias">Lei de Diretrizes Orçamentárias</option>
+            <option value="Plano Diretor">Plano Diretor</option>
+            <option value="Plano Municipal da Primeira Infância">Plano Municipal da Primeira Infância</option>
+            <option value="Plano Três Lagoas Sustentável">Plano Três Lagoas Sustentável</option>
+            <option value="Plano Setorial">Plano Setorial - Assistência Social</option>
+            
           </select>
         </label>
       </div>
@@ -130,7 +287,7 @@ const chartDataObj = {
       <div id="indicadores" className="bento-grid">
         <div className="bento-card" style={{ gridArea: "box-1" }}>
           <div className="grafico-status">
-            {chartDataObj ? <Doughnut data={chartDataObj} /> : <p>Carregando gráfico...</p>}
+            {chartDataObj ? <Doughnut data={chartDataObj} options={options_porcentagem} /> : <p>Carregando gráfico...</p>}
           </div>
         </div>
 
@@ -162,28 +319,48 @@ const chartDataObj = {
 
       <div style={{ marginTop: "2rem", padding: "1rem" }}>
         <h2 style={{ color: "#000",  }} >Lista de Metas</h2>
-        <div style={{ maxHeight: "400px", overflowY: "auto" }}>
+        <div className="listagem" >
           {dadosFiltrados.map((meta, i) => (
-            <div key={i} style={{
-              border: "1px solid #ccc",
-              borderRadius: "6px",
-              padding: "10px",
-              marginBottom: "10px",
-              background: "#fff",
-              color: "#000",
-              cursor: "pointer"
-            }} onClick={() => toggleLinha(i)}>
-              <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div key={i} className="unidade_lista"  onClick={() => toggleLinha(i)}>
+              <div style={{   display: "flex", justifyContent: "space-between" }}>
                 <div><strong>Nº:</strong> {meta.Numero}</div>
-                <div><strong>Status:</strong> {meta.Status}</div>
+               <div className="status" style={{
+  background: {
+   "Concluída": "rgba(0, 226, 0, 0.7)",
+  "Em Partes": "rgba(0, 96, 163, 0.7 )",
+  "Planejada": "rgba(255, 255, 0, 0.7)",
+  "Não Contemplada": "rgba(232, 36, 36, 0.7 )"
+  }[meta.Status],        
+  padding: "8px",
+  borderRadius: "5px",
+  color: "#000",
+ 
+
+
+
+ 
+}}>
+  <strong  >Status:</strong> {meta.Status}
+</div>
+
                 
               </div>
-              <div><strong>Descrição:</strong> {meta.Meta}</div>
-              <div><strong>Secretaria:</strong> {meta.Secretaria}</div>
+
+               <div style={{ marginTop: "1%",  display: "flex", justifyContent: "space-between" }}>
+             <div className="descricao"  ><strong>Descrição:</strong> {meta.Plano === "Plano de Governo" ? meta.Objetivo : meta.Meta}</div>
+               
+             <div style={{marginRight: "5px"}} ><strong>ODS Vinculados:</strong> {meta.ODS}</div>
                <div><strong>Conclusão:</strong> {meta.Data_conclusão}</div>
+               </div>
+                
+                <div style={{marginTop: "20px"}} ><strong>Secretaria:</strong> {meta.Secretaria}</div>
+
+
               {linhaAberta === i && (
+               
                 <div style={{ marginTop: "10px", background: "#f4f4f4", padding: "10px", borderRadius: "4px" }}>
-                  <strong>Detalhamento:</strong> {meta.Detalhamento || "Não informado"}
+                   
+                  <strong>Detalhamento:</strong> {meta.Plano != "Plano de Governo" ? meta.Detalhamento : meta.Meta}
                 </div>
               )}
             </div>
@@ -191,33 +368,160 @@ const chartDataObj = {
         </div>
       </div>
 
-      <div className="div_relatorios">
 
-        <h2>Relatórios Semestrais</h2>
+
+      <div className="div_financeiro">
+
+
+     
+       <div className="filtros_div">
+     <label>
+  Filtrar por Secretaria:
+  <br/>
+  <select className="filtros" value={filtroSecretaria} onChange={e => setFiltroSecretaria(e.target.value)}>
+    <option value="Todos">Todos</option>
+    
+    {Array.from(new Set(dados_financeiros.map(d => d.Secretaria).filter(Boolean))).map((s, i) => (
+      <option key={i} value={s}>{s}</option>
+    ))}
+  </select>
+</label>
+
+<label>
+  Filtrar por Tipo:
+  <br/>
+  <select className="filtros" value={filtroTipo} onChange={e => setFiltroTipo(e.target.value)}>
+    <option value="Geral">Geral</option>
+    <option value="Investimento">Investimento</option>
+  
+  </select>
+</label>
+
+</div>
+
+
+
+
+
+
+ 
+
+</div>
+
+   <div className="div_graficos" >
+     <div className="grafico-financeiro">
+  {dados_financeiros.length > 0 ? (
+  <Line data={dadosLinha} options={opcoesLinha} />
+  ) : (
+    <p>Carregando gráfico...</p>
+  )}
+</div>
+
+  <div className="grafico-financeiro">
+  {dados_financeiros.length > 0 ? (
+  <Bar data={dadosLinha} options={opcoesLinha} />
+  ) : (
+    <p>Carregando gráfico...</p>
+  )}
+</div>
+
+</div>
+
+
+
+    
+
+
+
+     <div style={{  marginTop: "4rem", padding: "1rem" }} >
+  <h2 style={{ color: "#000" }}>Lista de Dados Financeiros</h2>
+  <div className="listagem">
+    {dadosFinanceirosFiltrados.map((item, i) => (
+      <div
+        key={i}
+        style={{
+          border: "1px solid #ccc",
+          borderRadius: "6px",
+          padding: "10px",
+          marginBottom: "10px",
+          background: "#fff",
+          color: "#000",
+          cursor: "pointer",
+        }}
+        onClick={() => toggleLinha(i)}
+      >
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div><strong>Ano:</strong> {item.Ano}</div>
+          <div><strong>Tipo:</strong> {item.Tipo}</div>
+        </div>
+
+        <div style={{ display: "flex", justifyContent: "space-between", marginTop: "20px" }}>
+        <div><strong>Secretaria:</strong> {item.Secretaria}</div>
+        <div><strong>Dotação Inicial:</strong> R$ {item.Dotacao_Inicial}</div>
+        <div><strong>Dotação Final:</strong> R$ {item.Dotacao_Final}</div>
+         </div>
+
+     
+      </div>
+    ))}
+  </div>
+</div>
+
+
+
+      <div  id="secao-relatorios" className="div_relatorios">
+
+        <h2>Relatórios 1º Semestre</h2>
         <div className="relatorios">
-            <button className="botoes_relatorios" ><IoDocumentSharp /> 2025</button>
-            <button className="botoes_relatorios"> <IoDocumentSharp />2026 </button>
-            <button className="botoes_relatorios"> <IoDocumentSharp /> 2027</button>
-            <button className="botoes_relatorios" ><IoDocumentSharp />  2028</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1sowNiEokWTKiPUG2w595RJNSealPY5DZ/preview")} ><IoBusinessOutline className="icones_secretarias" /> <br/> Gestão e Inovação</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1SPJLBA-AT5oF12YzE-YJjH5Zh72Kr_33/preview")}> <LiaHatCowboySideSolid className="icones_secretarias" />  <br/>Agronegócio </button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1C69ud1Rf42gKQK6TG6O4NazXL_Oz_Xzd/preview")}> <FaHandsHelping className="icones_secretarias" /> <br/> Assistência Social</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1xH-D1YmFFrXYWHQ6kUddZmtG3APkD6ax/preview")}><FaTheaterMasks className="icones_secretarias" /> <br/> Cultura</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1tazA6kyOnNRWud0-E9tgEcV-72i6oTeG/preview")}> <IoTrendingUpOutline className="icones_secretarias" /> <br/>Desenvolvimento Econômico</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1kIBza8h86GjIK29ABb4e4R1T3NstwyoA/preview")}> <IoSchoolOutline className="icones_secretarias" /> <br/> Educação</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/17GEWy61JXAJcFaFbEzIuxV16m80dJS4u/preview")}><RiMoneyDollarCircleLine className="icones_secretarias" /> <br/>  Finanças</button>
+
+            
+        </div>
+
+        <div className="relatorios">
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1-v5n3_EzW00xqWayb9wcpaLCay1yqKDp/preview")}> <RiGovernmentLine className="icones_secretarias"/><br/> Governo </button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1zmqOKpqaTd3zRR0_Dm8tiBRcBQqdaM73/preview")}> <BsHouses className="icones_secretarias" /> <br/> Habitação</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/19JQXJ46s1MBQcCSGJiM_3te27FfXZHG3/preview")}><CiDeliveryTruck className="icones_secretarias" /> <br/>  Infraestrutura</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/14b4AyKBhYMAc1afkKhnsnEtEAB_C7pj-/preview")}> <FaLeaf className="icones_secretarias" /> <br/> Meio Ambiente </button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1I_X2FLDuM83DZyxb1olgwM7m7T1F4efU/preview")}> <MdHealthAndSafety className="icones_secretarias" /> <br/> Saúde</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1a2FlYr385ofTlQVXBb5gCmNu_w1mxoc5/preview")}><MdOutlineSportsVolleyball className="icones_secretarias" /> <br/>  Sejuvel</button>
+             <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1MdKwHi-FMfqSaa9OMFxGAZ_E_XHwPr0F/preview")}><PiTrafficSign className="icones_secretarias" /> <br/>  Transporte</button>
         </div>
       </div>
 
        <div className="div_relatorios">
 
-        <h2>"Instrumentos de Planejamento</h2>
+        <h2>Instrumentos de Planejamento</h2>
         <div className="relatorios">
-            <button className="botoes_relatorios" ><IoDocumentSharp />  2025</button>
-            <button className="botoes_relatorios"> <IoDocumentSharp />2025 </button>
-            <button className="botoes_relatorios"> <IoDocumentSharp /> 2025</button>
-            <button className="botoes_relatorios" > <IoDocumentSharp /> 2025</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1Dxx0vTkMO2zWpNIJDbdtgYGil5VpDWok/preview")} ><IoDocumentSharp className="icones_secretarias" /> <br/> Plano de Governo</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/18UHUPyS0VLzoeZUI7JlOdSuvwrSEKY48/preview")} > <IoDocumentSharp className="icones_secretarias" /> <br/>Plano Plurianual </button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/10igvaiyBnXKwflsXiDz46XCCNBtluNUc/preview")} > <IoDocumentSharp className="icones_secretarias" /> <br/> Plano Diretor</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1F9YYY1EqJJ6GTWantZFuTyKxAdGIbNJ-/preview")} > <IoDocumentSharp className="icones_secretarias" />  <br/>LDO 2025</button>
         </div>
          <div className="relatorios">
-            <button className="botoes_relatorios" >  <IoDocumentSharp /> 2025</button>
-            <button className="botoes_relatorios"> <IoDocumentSharp /> 2025 </button>
-            <button className="botoes_relatorios"> <IoDocumentSharp /> 2025</button>
-            <button className="botoes_relatorios" > <IoDocumentSharp /> 2025</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1IwSHjBiYK6mPeTQqPCLKjovbjIC42OXd/preview")} >  <IoDocumentSharp className="icones_secretarias" /> <br/> Plano Três Lagoas Sustentável</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1BbuQfYBInMoXSbcS5V9QJAF7KgJb4td_/preview")} > <IoDocumentSharp className="icones_secretarias" />  <br/>Plano de Capacitação </button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1HlgOdJiX8m4iM_xnGhmzWAYjMiMW9X3J/preview")} > <IoDocumentSharp className="icones_secretarias" /> <br/> Plano de Gestão de Risco</button>
+            <button className="botoes_relatorios" onClick={() => window.open("https://drive.google.com/file/d/1XvuVONLwJZdiBSkYHfVvHgtC3hkiMVp-/preview")}> <IoDocumentSharp  className="icones_secretarias"/> <br/> Planejamento Estratégico</button>
         </div>
+
+        
+</div>
+    
+
       </div>
+
+       <footer className="rodape">
+      <p>&copy; {new Date().getFullYear()} Prefeitura Municipal de Três Lagoas. Todos os direitos reservados.</p>
+       <p>Departamento de Planejamento e Estatística.</p>
+     </footer>
+         
     </>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,40 +1,61 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import './Home.css';
 
-import foto_paco from "../../assets/PACO-MUNICIPAL.jpg";
+import foto_paco from "../../assets/comite.jpeg";
 import logo_programa_metas from "../../assets/programa_metas_branco.png"
+import videoTL from "../../assets/video_treslagoas.mp4";
 
 const Home = () => {
-  useEffect(() => {
-    // Exemplo: pode colocar lógica para animações, fetch, etc.
-  }, []);
+  const [showModal, setShowModal] = useState(false);
 
+  
   return (
-    <main >
+    <main>
+      <div className="div_img-fundo"> 
+        <video src={videoTL} autoPlay muted loop className="img-fundo"></video>
 
-      <div className="div_img-fundo" > 
-      <img src={foto_paco} alt="logo programa de metas" className="img-fundo"/> 
+        <div className="conteudo_sobre_imagem">
+          <img src={logo_programa_metas} alt="logo programa de metas" className="logo_central"/>
 
-        
-      <div className="conteudo_sobre_imagem">
-
-
-         <img src={logo_programa_metas} alt="logo programa de metas" className="logo_central"/>
-
-        <div className="botoes" >
-
-          <button className="botao">Sobre o Programa de Metas</button>
-             <button className="botao"> Relatórios</button>
-
-
+          <div className="botoes">
+            <button className="botao" onClick={() => setShowModal(true)}>Sobre o Programa de Metas</button>
+          <button
+  className="botao"
+  onClick={() => {
+    const relatorios = document.getElementById("secao-relatorios");
+    if (relatorios) {
+      relatorios.scrollIntoView({ behavior: "smooth" });
+    }
+  }}
+>
+  Relatórios
+</button>
+          </div>
         </div>
-
-
-     
-
       </div>
-      </div>
-    
+
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <button className="modal-close"  onClick={() => setShowModal(false)}>×</button>
+            <h2>Sobre o Programa de Metas</h2>
+            <p>O Programa de Metas, instituído em 2019, é uma ferramenta estratégica voltada ao monitoramento 
+              e à análise estatística dos principais instrumentos de planejamento do município. Seu objetivo é
+               assegurar que as metas, os objetivos e as prioridades definidos pelo governo municipal, em 
+               conjunto com a população, sejam efetivamente alcançados. Além disso, o programa reforça o compromisso com a transparência e a eficiência na gestão pública.
+            </p>
+            <img src={foto_paco} alt="Foto Paço Municipal" className="modal-img" />
+              <p>A condução do Programa de Metas é responsabilidade do Departamento de Planejamento e Estatística, que lidera o Comitê de Monitoramento e Avaliação.
+                 Esse comitê é composto por representantes de todas as secretarias municipais, garantindo uma abordagem integrada e colaborativa na formulação, 
+                 acompanhamento e avaliação das políticas públicas.</p>
+
+                  <p>Para além das atividades de monitoramento, o comitê também atua diretamente na elaboração dos instrumentos de planejamento, como o Plano Plurianual (PPA), 
+                    a Lei de Diretrizes Orçamentárias (LDO) e a Lei Orçamentária Anual (LOA). Sua atuação vai além do acompanhamento de metas: envolve a implantação e o aprimoramento 
+                    contínuo de estratégias de gestão e governança, sempre alinhadas às diretrizes do modelo Gestão Pública Gov, que orienta a administração municipal na busca por resultados 
+                    mais efetivos, inovação na gestão e maior participação social.</p>
+          </div>
+        </div>
+      )}
     </main>
   );
 };
